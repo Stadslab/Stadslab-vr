7542760fa763d0f5be66b6dc76118ebc8a9c1d2f
Maarten Knolle <maarten.knolle@gmail.com>
2022-11-02 16:54:01 +0100

    simple pistol added

---
+++ Assets/Assets/FIreBulletOnActive.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class FIreBulletOnActive : MonoBehaviour
+{
+    public GameObject bullet;
+    public Transform spawnPoint;
+    public float fireSpeed = 20;
+    public Quaternion rotationAmount = Quaternion.Euler(90, 0, 0);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        XRGrabInteractable grabbable = GetComponent<XRGrabInteractable>();
+        grabbable.activated.AddListener(FireBullet);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void FireBullet(ActivateEventArgs args)
+    {
+        GameObject spawnBullet = Instantiate(bullet);
+        spawnBullet.transform.position = spawnPoint.position;
+        Quaternion rotationAmount = Quaternion.Euler(90, 0, 0);
+        spawnBullet.transform.rotation = spawnPoint.rotation * rotationAmount; ;
+        spawnBullet.GetComponent<Rigidbody>().velocity = spawnPoint.forward * fireSpeed;
+        Destroy(spawnBullet, 10);
+    }
+}
